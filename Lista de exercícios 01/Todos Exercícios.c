261f0a70651611a3f0a4d0a8562783afb37e4384
souzajulen-ui <souzajulen@gmail.com>
2026-09-23 17:11:58 -0300

    Refactor and add exercises in Todos Exercícios.c

--- Lista de exercícios 01/Todos Exercícios.c
+++ Lista de exercícios 01/Todos Exercícios.c
@@ -82,3 +82,99 @@ float salarioFixo, totalVendas, totalReceber;
 
     return 0;
 }
+
+//Exercício 5
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int v1, v2, v3, v4;
+    int soma, produtorio;
+    float media;
+
+    printf("Digite 4 valores inteiros: ");
+    scanf("%d %d %d %d", &v1, &v2, &v3, &v4);
+
+    soma = v1 + v2 + v3 + v4;
+    media = soma / 4.0f; 
+    produtorio = v1 * v2 * v3 * v4;
+
+    printf("Soma: %d\n", soma);
+    printf("Media: %.2f\n", media);
+    printf("Produtorio: %d\n", produtorio);
+
+    return 0;
+}
+
+//Exercício 6
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int idade;
+    int anos, meses, dias;
+    
+	printf("Digite a idade em dias: ");
+    scanf("%d", &idade);
+
+    anos = idade / 365;
+    idade = idade % 365;
+
+    meses = idade / 30;
+    dias = idade % 30;
+
+    printf("%d ano(s)\n", anos);
+    printf("%d mes\n", meses);
+    printf("%d dia(s)\n", dias);
+
+    return 0;
+}
+
+//Exercício 7
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    float raio, volume;
+    float pi = 3.14159f;
+
+    printf("Digite o valor do raio da esfera: ");
+    scanf("%f", &raio);
+
+    volume = (4.0f / 3.0f) * pi * (raio * raio * raio);
+
+    printf("VOLUME = %.3f\n", volume);
+
+    return 0;
+}
+
+//Exercício 8
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+
+    float x1, y1, x2, y2;
+    float dx, dy, distancia;
+
+    printf("Digite as coordenadas x1 e y1 do P1: ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Digite as coordenadas x2 e y2 do P2: ");
+    scanf("%f %f", &x2, &y2);
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    distancia = sqrtf((dx * dx) + (dy * dy));
+
+    printf("Distancia: %.4f\n", distancia);
+
+    return 0;
+}
