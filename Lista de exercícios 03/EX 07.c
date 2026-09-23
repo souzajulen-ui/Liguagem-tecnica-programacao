29d5f11c5367063bb12893c15fa728b2d5d41368
souzajulen-ui <souzajulen@gmail.com>
2026-09-23 16:16:22 -0300

    Add EX 07.c

---
+++ Lista de exercícios 03/EX 07.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float inss(float bruto) {
+    if (bruto <= 1412.00) return bruto * 0.075;
+    else if (bruto <= 2666.68) return bruto * 0.09;
+    else if (bruto <= 4000.03) return bruto * 0.12;
+    else return bruto * 0.14;
+}
+
+int main() {
+    float bruto;
+
+    printf("Salario bruto: ");
+    scanf("%f", &bruto);
+    printf("INSS: %.2f\n", inss(bruto));
+
+    return 0;
+}
