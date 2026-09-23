f474efdb7f111b56c78f2d50ab3c4c565c94d090
souzajulen-ui <souzajulen@gmail.com>
2026-09-23 17:06:20 -0300

    Add exercises 2 and 3 to EX 1.c
    Added exercises 2 and 3 to the existing C program.

--- Lista de exercícios 01/EX 1.c
+++ Lista de exercícios 01/EX 1.c
@@ -15,3 +15,27 @@ int main(int argc, char *argv[]) {
     
 	return 0;
 }
+
+//exercício 2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    double valor;
+
+    printf("Digite um valor positivo: ");
+    scanf("%lf", &valor);
+
+    if (valor <= 0) {
+        printf("Por favor, insira apenas valores positivos.\n");
+        return 1;
+    }
+
+    printf("Notacao cientifica: %e\n", valor);
+
+    return 0;
+}
+
+//exercício 3
